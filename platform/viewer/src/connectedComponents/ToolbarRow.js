53d69ccfd271182f0f75eec2ed89c5b35cff5988
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2021-03-15 12:43:08 -0300

    GUI-0.017
    add layout button

--- platform/viewer/src/connectedComponents/ToolbarRow.js
+++ platform/viewer/src/connectedComponents/ToolbarRow.js
@@ -193,7 +193,7 @@ class ToolbarRow extends Component {
             />
           </div>
           {buttonComponents}
-          {/* <ConnectedLayoutButton /> */}
+          <ConnectedLayoutButton />
           <div
             className="pull-right m-t-1 rm-x-1"
             style={{ marginLeft: 'auto' }}
